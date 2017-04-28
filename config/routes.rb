c87d9e4d65007ee22c006dbe2a262eabc04e9f27
Danish Mughal <mughal.danish@gmail.com>
2017-04-27 18:12:19 -0700

    Added person model, people api controller, and index jbuilder view

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: "pages#home"
+  
+
+  namespace :api do
+    namespace :v1 do 
+      get "/people", to: "people#index"
+    end
+  end
+
 end
